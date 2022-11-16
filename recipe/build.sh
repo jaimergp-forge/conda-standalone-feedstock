b9b612bf9df36340888c3f37d64877e77f02076b
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-11-16 12:48:38 +0100

    get codesign out of the way

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,8 @@ for fname in "core/path_actions.py" "utils.py"; do
   cp conda_src/conda/${fname} $SP_DIR/conda/${fname}
 done
 
+# a dependency has a codesign executable that shadows Apple's codesign
+mv "$PREFIX/bin/codesign" "$PREFIX/bin/codesign.bak"  || true
 
 # -F is to create a single file
 # -s strips executables and libraries
@@ -12,3 +14,5 @@ mkdir -p $PREFIX/standalone_conda
 mv dist/conda.exe $PREFIX/standalone_conda
 # clean up .pyc files that pyinstaller creates
 rm -rf $PREFIX/lib
+
+mv "$PREFIX/bin/codesign.bak" "$PREFIX/bin/codesign"  || true
